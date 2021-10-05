48d4f6c4e2a6a108ad953d28ab6424fa8a25508d
mhoncharuk <mhoncharuk@geeksforless.net>
2021-10-05 22:15:22 +0300

    Fixed test script

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,5 +1,5 @@
 # !/bin/bash
 
-cd /home/foldermonitor && ./foldermonitor
+cd /home/foldermonitor && ./daemon-foldermonitor
 cp -a /home/foldermonitor/testfiles/test* /home/foldermonitor/monitorme
 sqlite3 /home/foldermonitor/database/foldermonitor.db "select * from filedata"
